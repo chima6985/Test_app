03a3bc39dbdeb358720b807c077cf87c05edc1ad
chima6985 <chimaokeke2006@gmail.com>
2025-03-19 12:29:51 +0100

    woring on the Signup page

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,13 +1,56 @@
 import 'package:flutter/material.dart';
-class SignupScreen extends StatelessWidget{
+
+class SignupScreen extends StatelessWidget {
   SignupScreen({super.key});
   final Color singupScreenColor = Color(0XFFECF4E2);
   @override
   Widget build(BuildContext context) {
-  return Scaffold(
-    body: Container(decoration: BoxDecoration(
-      color: singupScreenColor
-    ),)
-  );
+    final mqr = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          margin: EdgeInsets.all(0),
+          width: mqr.width,
+          height: mqr.height,
+          decoration: BoxDecoration(color: singupScreenColor),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(0XFFE5E5E5),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      spreadRadius: 2,
+                      offset: Offset(0, 0),
+                    ),
+                  ],
+                ),
+                child: Icon(
+                  Icons.arrow_back,
+                  size: 42,
+                  color: Color(0XFF3C6255),
+                ),
+              ),
+              SizedBox(height: 25),
+              Center(
+                child: Text(
+                  'Create Your account',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0XFF3E3E3E),
+                  ),
+                ),
+              ),
+              SizedBox(height: 25),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
