6ea12a9b3bc43c4f027c734062e886c76efc33f3
chima6985 <chimaokeke2006@gmail.com>
2025-04-02 09:46:10 +0100

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:test_application/screens/landing_page.dart';
 // import 'package:test_application/screens/home_page.dart';
 import 'package:test_application/screens/splash_screen.dart';
 
@@ -22,6 +23,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: LandingPage());
   }
 }
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,18 +1,18 @@
-import 'package:flutter/material.dart';
-import 'package:animated_splash_screen/animated_splash_screen.dart';
-import 'package:test_application/screens/home_page.dart';
-import 'package:lottie/lottie.dart';
+// import 'package:flutter/material.dart';
+// import 'package:animated_splash_screen/animated_splash_screen.dart';
+// import 'package:test_application/screens/home_page.dart';
+// import 'package:lottie/lottie.dart';
 
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
+// class SplashScreen extends StatelessWidget {
+//   const SplashScreen({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedSplashScreen(
-      splash: LottieBuilder.asset('assets/Animation - 1742915240013.json'),
-      nextScreen: MyHomePage(),
-      splashIconSize: 300,
-      backgroundColor: Color(0XFFECF4E2),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return AnimatedSplashScreen(
+//       splash: LottieBuilder.asset('assets/Animation - 1742915240013.json'),
+//       nextScreen: MyHomePage(),
+//       splashIconSize: 300,
+//       backgroundColor: Color(0XFFECF4E2),
+//     );
+//   }
+// }
