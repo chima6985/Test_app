f696d435972511b634ab30fba1855f45f5975607
chima6985 <chimaokeke2006@gmail.com>
2025-04-02 11:07:34 +0100

    key value system

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:test_application/screens/home_page.dart';
+import 'package:test_application/screens/signin_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,6 +13,9 @@ void main() async {
     messagingSenderId: 'your-messaging-sender-id',
     projectId: 'test-app-6c949',
   );
+
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool? isFirstTime = prefs.getBool('is first time');
 
   await Firebase.initializeApp(options: options);
   runApp(MyApp());
